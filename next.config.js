1e8cd88f71cb69cde5a87b996feff4bfca7e1005
gburry-fireblocks <gburry@fireblocks.com>
2023-02-11 02:09:27 -0500

    feat: add pwa support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
+// @ts-check
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  reloadOnOnline: false,
+});
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   /** We run eslint as a separate task in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
 };
+
+// @ts-ignore
+module.exports = withPWA(nextConfig);
