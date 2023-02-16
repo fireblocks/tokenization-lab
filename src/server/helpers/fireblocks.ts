df6093c92d7066161c36a8096e55020bb7e37111
gburry-fireblocks <gburry@fireblocks.com>
2023-02-16 17:34:52 -0500

    fix: test API secret key

--- src/server/helpers/fireblocks.ts
+++ src/server/helpers/fireblocks.ts
@@ -4,19 +4,28 @@ import { FireblocksSDK } from "fireblocks-sdk";
 
 const PRIVATE_KEY_B64 = process.env.PRIVATE_KEY_B64;
 
+const PRIVATE_KEY_ERROR = new Error("No API private key found");
+
 export const getFireblocks = async (apiKey: string) => {
   let privateKey: string;
 
-  if (PRIVATE_KEY_B64) {
-    privateKey = Buffer.from(PRIVATE_KEY_B64, "base64").toString("utf-8");
-  } else {
-    const privateKeyPath = path.resolve(process.cwd(), "keys", "api.key");
+  console.info(
+    "Is private key env var set?",
+    PRIVATE_KEY_B64?.length ? "Yes" : "No"
+  );
 
+  const privateKeyPath = path.resolve(process.cwd(), "keys", "api.key");
+
+  if (PRIVATE_KEY_B64?.length) {
+    privateKey = Buffer.from(PRIVATE_KEY_B64, "base64").toString("utf-8");
+  } else if (fs.existsSync(privateKeyPath)) {
     privateKey = await fs.promises.readFile(privateKeyPath, "utf-8");
+  } else {
+    throw PRIVATE_KEY_ERROR;
   }
 
   if (!privateKey) {
-    throw new Error("No API private key found");
+    throw PRIVATE_KEY_ERROR;
   }
 
   const fireblocks = new FireblocksSDK(privateKey, apiKey);
