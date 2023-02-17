e0786ff80689ddaaab56c2fc70de640f493c5fbd
gburry-fireblocks <gburry@fireblocks.com>
2023-02-17 16:28:17 -0500

    fix: dismiss notification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -343,6 +343,7 @@ const Index = ({ hasApiPrivateKey }: Props) => {
       <div className="flex justify-center py-3">
         <Link
           href="/deploy"
+          onClick={onCloseNotification}
           className={clsx(
             "text-md inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-3 px-5 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2",
             (assetsMutation.isLoading ||
